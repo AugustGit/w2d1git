6481b529b6d3ffaf6c6e0870095a4095143bb74f
austin metka <austin_metka@hotmail.com>
2018-01-22 17:27:05 +0000

    printingInChunks written attempt to break up data chunks 1

---
+++ printingInChunks.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks() {
+
+  var requestOptions = {
+    host:'sytantris.github.io',
+    path: 'http-examples/step1.html'
+  };
+
+  /* add code */
+https.get(requestOptions, function (response) {
+
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length',data , '\n' , data.length);
+  });
+
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+});
+
+}
